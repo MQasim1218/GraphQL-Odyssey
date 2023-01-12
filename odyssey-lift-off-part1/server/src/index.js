97d53cd15e31848927245d6c0a7eabbf991ba053
ahsansaif47@ <ahsansaif47@hotmail.com>
2023-01-12 17:08:14 +0500

    setting up appolo server

--- odyssey-lift-off-part1/server/src/index.js
+++ odyssey-lift-off-part1/server/src/index.js
@@ -1,1 +1,33 @@
-//TODO
+// TODO: Build ur Fisrt Apollo Server!
+
+const ApolloServer = require('apollo-server').ApolloServer
+const GQL_Types = require('./schema')
+
+// This impl of mocks returns realistic data based on our logic
+const mocks = {
+    Track: () => ({
+        id: () => 20,
+        title: () => 'CattyWalks',
+        author: () => ({
+            id: () => 2,
+            name: () => 'grumpy'
+        }),
+        lengthInMins: () => 12,
+    }),
+    Author: () => ({
+        id: () => 20,
+        title: () => 'CattyWalks',
+    })
+}
+
+const server = new ApolloServer({
+    typeDefs: GQL_Types,
+    mocks: true
+})
+
+server.listen().then(() => {
+    console.log(`
+        🛫 Server Up and Running
+        🃏 Listening on port http://localhost:4000/ 
+    `)
+})
---
+++ odyssey-lift-off-part1/server/src/schema.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server')
+
+module.exports = GQL_Types = gql`
+
+    type Query {
+        homepageTracks: [Track!]!
+        spaceCats: [SpaceCat]
+    }
+
+    type Track {
+        id: Int!
+        title: String!
+        author: Author!
+        lengthInMins: Int,
+        "URL to the file location!"
+        thumbnail: String
+        modulesCount: Int
+    }
+
+    type Author{
+        id: ID!
+        name: String!
+        photo: String
+    }
+
+    type SpaceCat {
+        id: ID!
+        name: String!
+        age: Int
+        missions:[Mission]
+    }
+
+    type Mission {
+        id: ID!
+        name: String!
+        description: String!
+    }
+`
